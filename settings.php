a9d17e17863e978b81059ee019c464d20ca3ce8c
OzzyFin <ozzfi1@gmail.com>
2015-12-24 16:30:04 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,9 +3,9 @@
 		// TWITTER OAUTH SETTINGS //
 		// THESE ARE FOUND FROM "Keys and Access Tokens" TAB IN YOUR TWITTER APPLICATION //
 		"oauth_access_token" => "",
-		"oauth_access_token_secret" => "wsm84P0hOTFwX2AzJ9ryXuuz7uyp15wn06FnqK4gAkcDc",
-		"consumer_key" => "HkfNpmBNbJYFIuuaWICYGwb8f",
-		"consumer_secret" => "GZhRcxM41kifvv4tz3KcQI3le9LNQDsE1HxcCP79hpYqiJeZgA",
+		"oauth_access_token_secret" => "",
+		"consumer_key" => "",
+		"consumer_secret" => "",
 		
 		// OTHER STUFF //
 		"secretkey" => "123456789" // you don't want random people to tweet random stuff on your account, right? In case you are using my ModuleScript on ROBLOX remember to set secretkey to the same value as here 
