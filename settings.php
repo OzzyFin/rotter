f6585e9ce4a0fabe6e875c1e5b7174a6be3b066c
OzzyFin <ozzfi1@gmail.com>
2015-12-25 14:42:51 +0200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+	$settings = array( 
+		// TWITTER OAUTH SETTINGS //
+		"oauth_access_token" => "",
+		"oauth_access_token_secret" => "",
+		"consumer_key" => "",
+		"consumer_secret" => "",
+		
+		// OTHER STUFF //
+		"secretkey" => "123456789" // you don't want random people to tweet random stuff on your account, right? In case you are using my ModuleScript on ROBLOX remember to set secretkey to the same value as here 
+	);
+?>
