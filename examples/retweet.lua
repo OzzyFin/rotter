04a67b072c50b07f5a84715a469281ad3635908a
OzzyFin <ozzfi1@gmail.com>
2015-12-25 19:43:18 +0200

    Update retweet.lua

--- examples/retweet.lua
+++ examples/retweet.lua
@@ -1,6 +1,6 @@
 local twitter = require(script.Parent.twitterModule)
 
-local result = twitter:Request("POST","statuses/retweet.json",
+twitter:Request("POST","statuses/retweet.json",
 	{
 		id = "679029679123210240"
 	}
